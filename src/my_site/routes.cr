84ecd38df6a3f4cbbc024c81b1a51f4c6ef15aa6
kebasyaty <kebasyaty@gmail.com>
2024-05-21 19:42:58 +0300

    update

--- src/my_site/routes.cr
+++ src/my_site/routes.cr
@@ -4,6 +4,22 @@ module Vizbor::Server::Routes
     "Hello World!"
   end
 
+  get "/favicon.ico" do
+    # ...
+  end
+
+  get "/favicons/:icon" do
+    # ...
+  end
+
+  get "/robots.txt" do
+    # ...
+  end
+
+  get "/sitemap.xml" do
+    # ...
+  end
+
   error 404 do
     "This is a customized 404 page."
   end
