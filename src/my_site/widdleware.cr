48ffd900b7cddaebc604df9cf58b41d1e87cb3f9
kebasyaty <kebasyaty@gmail.com>
2024-05-22 12:03:06 +0300

    update

--- src/my_site/widdleware.cr
+++ src/my_site/widdleware.cr
@@ -4,14 +4,14 @@ module Vizbor::Middleware
   # https://github.com/kemalcr/kemal-session
   Kemal::Session.config do |config|
     # How long is the session valid after last user interaction?
-    config.timeout = Time::Span.new(1, 0, 0) # 1 hour
+    config.timeout = 1.hours
     # Name of the cookie that holds the session_id on the client.
     config.cookie_name = "kemal_sessid"
     # How are the sessions saved on the server?
     # https://github.com/kemalcr/kemal-session#setting-the-engine
     config.engine = Kemal::Session::MemoryEngine.new
     # In which interval should the garbage collector find and delete expired sessions from the server?
-    config.gc_interval = Time::Span.new(0, 4, 0) # 4 minutes
+    config.gc_interval = 4.minutes
     # Used to sign the session ids before theyre saved in the cookie.
     config.secret = Vizbor::Settings.secret_key
     # The cookie used for session management should only be transmitted over encrypted connections.
