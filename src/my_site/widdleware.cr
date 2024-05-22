32c76ab4ec9babd7562248dee1b7117684eb5a72
kebasyaty <kebasyaty@gmail.com>
2024-05-22 11:14:59 +0300

    update

--- src/my_site/widdleware.cr
+++ src/my_site/widdleware.cr
@@ -1,11 +1,13 @@
 # https://kemalcr.com/guide/#middleware
 module Vizbor::Middleware
+  # CSRF
+  # https://github.com/kemalcr/kemal-csrf
   add_handler CSRF.new(
     header: "X_CSRF_TOKEN",
     allowed_methods: ["GET", "HEAD", "OPTIONS", "TRACE"],
-    allowed_routes: ["/api/somecallback", "/api/v1/**"],
-    parameter_name: "_csrf",
-    error: "CSRF Error",
+    parameter_name: "authenticity_token",
+    error: "Forbidden (CSRF)",
+    allowed_routes: [] of String,
     http_only: false,
     samesite: nil,
   )
