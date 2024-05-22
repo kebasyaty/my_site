9577b18121ec82e5070654b96b44abfac8448ebe
kebasyaty <kebasyaty@gmail.com>
2024-05-22 09:30:13 +0300

    update

--- src/my_site/headers.cr
+++ src/my_site/headers.cr
@@ -3,6 +3,10 @@ module Vizbor::StaticHeaders
   # This is especially useful for stuff like CORS or caching.
   static_headers do |response, filepath, filestat|
     response.headers.add("Access-Control-Allow-Origin", Vizbor::Settings.domain_name)
+    response.headers.add("Access-Control-Allow-Methods", "GET")
+    response.headers.add("Access-Control-Allow-Headers", "content-type,authorization,accept")
+    response.headers.add("Access-Control-Max-Age", Vizbor::Settings.debug? ? "-1" : "3600")
+    response.headers.add("", "")
     #
     response.headers.add("Content-Size", filestat.size.to_s)
     response.headers.add("X-XSS-Protection", "1; mode=block")
