6ae696207e9154187b267ce24f45dca708eb8b14
kebasyaty <kebasyaty@gmail.com>
2024-06-04 16:52:56 +0300

    update

--- src/my_site/settings.cr
+++ src/my_site/settings.cr
@@ -2,6 +2,25 @@
 module Vizbor::Settings
   extend self
 
+  # KEMAL PARAMETERS
+  # ----------------------------------------------------------------------------
+  # Static File Options.
+  # https://kemalcr.com/guide/
+  # Example: {"gzip" => true, "dir_listing" => false}
+  class_getter! static_file_options : Hash(String, Bool)
+  # Disable Static Files.
+  class_getter? disable_static_files : Bool = false
+  # Using Reuse Port for Multiple Kemal Processes.
+  # https://kemalcr.com/cookbook/reuse_port/
+  class_getter? server_reuse_port : Bool = false
+  # Use Logging?
+  # https://kemalcr.com/guide/
+  # You can add logging statements to your code:
+  # Example: Log.info { "Log message with or without embedded #{variables}" }
+  class_getter? use_logging : Bool = true
+
+  # VIZBOR PARAMETERS
+  # ----------------------------------------------------------------------------
   # If true,
   # an exception page is rendered when an exception is raised which provides a
   # lot of useful information for debugging.
@@ -20,23 +39,6 @@ module Vizbor::Settings
   # To generate a key (This is not an advertisement): https://randompasswordgen.com/
   # Minimum 64 characters.
   class_getter secret_key : String = "d421a07ee91de270852641a9689a8e864e549b9d8bdfaa5785801fab0e3da311d6e64b5b21566095f5a19aa9d0c69efe0972473ec9b825247981ed57c31635a2"
-
-  # KEMAL PARAMETERS
-  # ----------------------------------------------------------------------------
-  # Static File Options.
-  # https://kemalcr.com/guide/
-  # Example: {"gzip" => true, "dir_listing" => false}
-  class_getter! static_file_options : Hash(String, Bool)
-  # Disable Static Files.
-  class_getter? disable_static_files : Bool = false
-  # Using Reuse Port for Multiple Kemal Processes.
-  # https://kemalcr.com/cookbook/reuse_port/
-  class_getter? server_reuse_port : Bool = false
-  # Use Logging?
-  # https://kemalcr.com/guide/
-  # You can add logging statements to your code:
-  # Example: Log.info { "Log message with or without embedded #{variables}" }
-  class_getter? use_logging : Bool = true
 
   # URI Scheme
   def scheme : String
