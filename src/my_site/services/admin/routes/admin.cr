d502fcb7139b033779cb25025683459de4015bdf
kebasyaty <kebasyaty@gmail.com>
2024-06-05 12:53:48 +0300

    update /sign-in

--- src/my_site/services/admin/routes/admin.cr
+++ src/my_site/services/admin/routes/admin.cr
@@ -5,7 +5,20 @@ module Vizbor::Services::Admin::Routes
   end
 
   # Login page
-  get "/sign-in" do
-    "Hello Admin!"
+  get "/sign-in" do |env|
+    if (user = env.session.object?("user")).nil?
+      if Vizbor::Services::Admin::Models::User.estimated_document_count == 0
+        # Create first user (administrator)
+        first_user = Vizbor::Services::Admin::Models::User.new
+        first_user.username.value = "admin"
+        first_user.email.value = "no_reply@email.net"
+        first_user.password.value = "12345678"
+        first_user.confirm_password.value = "12345678"
+        first_user.is_admin.value = true
+        first_user.is_active.value = true
+
+        first_user.print_err unless first_user.save
+      end
+    end
   end
 end
