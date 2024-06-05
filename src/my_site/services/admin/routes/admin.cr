2d59cff572d833deadf42ee8c33e4bf82ea26254
kebasyaty <kebasyaty@gmail.com>
2024-06-05 19:01:49 +0300

    update

--- src/my_site/services/admin/routes/admin.cr
+++ src/my_site/services/admin/routes/admin.cr
@@ -6,7 +6,9 @@ module Vizbor::Services::Admin::Routes
 
   # Get language code
   get "/language-code" do |env|
+    language_code = {language_code: "en", msg_err: ""}.to_json
     env.response.content_type = "application/json"
+    language_code
   end
 
   # Login page
