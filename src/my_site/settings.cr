c4df9d09bba87196de3c8f02722e01f5cf4c925e
kebasyaty <kebasyaty@gmail.com>
2024-06-06 09:01:45 +0300

    update

--- src/my_site/settings.cr
+++ src/my_site/settings.cr
@@ -5,18 +5,18 @@ module Vizbor::Settings
   # KEMAL PARAMETERS
   # ----------------------------------------------------------------------------
   # Static File Options.
-  # https://kemalcr.com/guide/
-  # Example: {"gzip" => true, "dir_listing" => false}
+  # NOTE: https://kemalcr.com/guide/
+  # NOTE: Example: {"gzip" => true, "dir_listing" => false}
   class_getter! static_file_options : Hash(String, Bool)
   # Disable Static Files.
   class_getter? disable_static_files : Bool = false
   # Using Reuse Port for Multiple Kemal Processes.
-  # https://kemalcr.com/cookbook/reuse_port/
+  # NOTE: https://kemalcr.com/cookbook/reuse_port/
   class_getter? server_reuse_port : Bool = false
   # Use Logging?
-  # https://kemalcr.com/guide/
-  # You can add logging statements to your code:
-  # Example: Log.info { "Log message with or without embedded #{variables}" }
+  # NOTE: https://kemalcr.com/guide/
+  # NOTE: You can add logging statements to your code:
+  # NOTE: Example: Log.info { "Log message with or without embedded #{variables}" }
   class_getter? use_logging : Bool = true
 
   # VIZBOR PARAMETERS
@@ -25,27 +25,27 @@ module Vizbor::Settings
   # an exception page is rendered when an exception is raised which provides a
   # lot of useful information for debugging.
   class_getter? debug : Bool = true
-  # Maximum 44 characters.
+  # WARNING: Maximum 44 characters.
   class_getter app_name : String = "MySite"
-  # Match regular expression: /^[a-zA-Z0-9]{16}$/
-  # To generate a key (This is not an advertisement): https://randompasswordgen.com/
+  # WARNING: Match regular expression: /^[a-zA-Z0-9]{16}$/
+  # NOTE: To generate a key (This is not an advertisement): https://randompasswordgen.com/
   class_getter unique_app_key : String = "f7XF38NO8JQG0Yu8"
-  # Maximum 60 characters.
-  # Hint: If the line is empty, the name will be generated automatically.
+  # WARNING: Maximum 60 characters.
+  # WARNING: If the line is empty, the name will be generated automatically.
   class_getter database_name : String = ""
-  # https://github.com/crystal-i18n/i18n
+  # NOTE: https://github.com/crystal-i18n/i18n
   class_getter default_locale : Symbol = :en
   # Security
-  # To generate a key (This is not an advertisement): https://randompasswordgen.com/
-  # Minimum 64 characters.
+  # WARNING: Minimum 64 characters.
   class_getter secret_key : String = "d421a07ee91de270852641a9689a8e864e549b9d8bdfaa5785801fab0e3da311d6e64b5b21566095f5a19aa9d0c69efe0972473ec9b825247981ed57c31635a2"
 
   # Administrator production email.
-  # Maximum 320 characters.
+  # WARNING: Maximum 320 characters.
   admin_prod_email = "???"
   # Administrator production password.
-  # To generate a key (This is not an advertisement): https://randompasswordgen.com/
-  # Maximum 256 characters.
+  # WARNING: Valid characters by default: a-z A-Z 0-9 - . _ ! " ` ' # % & , : ; < > = @ { } ~ $ ( ) * + / \ ? [ ] ^ |
+  # WARNING: Default number of characters: max = 256, min = 8
+  # NOTE: To generate a key (This is not an advertisement): https://randompasswordgen.com/
   admin_prod_pass = "???"
 
   # URI Scheme
