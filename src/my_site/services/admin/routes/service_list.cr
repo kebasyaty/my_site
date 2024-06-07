cfe1730125cecd323b397213ae8af1b1f4af717b
kebasyaty <kebasyaty@gmail.com>
2024-06-07 15:54:49 +0300

    update

--- src/my_site/services/admin/routes/service_list.cr
+++ src/my_site/services/admin/routes/service_list.cr
@@ -1,10 +1,29 @@
 module Vizbor::Services::Admin::Routes
-  # Logout
+  # Get service list
   post "/admin/service-list" do |env|
-    if !env.session.object?("user").nil?
-      env.session.destroy
+    is_authenticated : Bool = false
+    msg_err : String = ""
+
+    if !(user = env.session.object?("user")).nil?
+      user = user.as(UserStorableObject)
+      if !user.username.empty? &&
+         !user.hash.empty? && user.is_admin? && user.is_active?
+        is_authenticated = true
+      else
+        msg_err = I18n.t(:auth_failed)
+      end
+    else
+      msg_err = I18n.t(:auth_failed)
     end
-    result = {msg: "Goodbye!"}.to_json
+
+    result = {
+      is_authenticated: is_authenticated,
+      logo:             "",
+      brand:            "",
+      slogan:           "",
+      service_list:     "",
+      msg_err:          msg_err,
+    }.to_json
     env.response.content_type = "application/json"
     result
   end
