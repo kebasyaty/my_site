79eae9e750b88c502054ecf52132c544d51c0e0c
kebasyaty <kebasyaty@gmail.com>
2024-06-07 16:30:09 +0300

    update

--- src/my_site/services/admin/composition.cr
+++ src/my_site/services/admin/composition.cr
@@ -1,15 +1,15 @@
 # Service structure for the admin panel.
 module Vizbor::Services::Admin::Composition
-  type ServiceList = [
-    {
-      service:     {title: String, icon: String},
-      collections: [
-        {
-          title:     String,
-          model_key: String,
-          fields:    [{field: String, title: String}],
-        },
-      ],
-    },
-  ]
+  # type ServiceList = [
+  #   {
+  #     service:     {title: String, icon: String},
+  #     collections: [
+  #       {
+  #         title:     String,
+  #         model_key: String,
+  #         fields:    [{field: String, title: String}],
+  #       },
+  #     ],
+  #   },
+  # ]
 end
