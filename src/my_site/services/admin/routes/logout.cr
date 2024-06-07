4336c0585cedd8236ecbaa264222a5d049af888e
kebasyaty <kebasyaty@gmail.com>
2024-06-07 15:11:45 +0300

    update

--- src/my_site/services/admin/routes/logout.cr
+++ src/my_site/services/admin/routes/logout.cr
@@ -2,9 +2,9 @@ module Vizbor::Services::Admin::Routes
   # Logout
   post "/admin/logout" do |env|
     if !env.session.object?("user").nil?
-      # ...
+      env.session.destroy
     end
-    result = {msg: "Goodbye!"}.to_json
+    result = {msg: "You have been logged out."}.to_json
     env.response.content_type = "application/json"
     result
   end
