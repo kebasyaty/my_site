ee7037418688e9731138ce440ebf273a819cc9dc
kebasyaty <kebasyaty@gmail.com>
2024-06-07 15:05:09 +0300

    update

--- src/my_site/services/admin/routes/logout.cr
+++ src/my_site/services/admin/routes/logout.cr
@@ -1,6 +1,9 @@
 module Vizbor::Services::Admin::Routes
   # Logout
   post "/admin/logout" do |env|
+    if !env.session.object?("user").nil?
+      # ...
+    end
     result = {msg: "Goodbye!"}.to_json
     env.response.content_type = "application/json"
     result
