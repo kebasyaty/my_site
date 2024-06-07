abfd0ee23b237e05f5c154a94391cfaa42b1e22c
kebasyaty <kebasyaty@gmail.com>
2024-06-07 21:08:45 +0300

    update compose

--- src/my_site/services/admin/compose.cr
+++ src/my_site/services/admin/compose.cr
@@ -1,4 +1,14 @@
 # Service composition for the admin panel menu structure.
 module Vizbor::Services::Admin::Compose
-  # ...
+  composition = {
+    service:     {title: "", icon: ""},
+    collections: [
+      {
+        title:     "",
+        model_key: "",
+        fields:    [{field: "", title: ""}],
+      },
+    ],
+  }
+  Vizbor::Composition.add(composition)
 end
