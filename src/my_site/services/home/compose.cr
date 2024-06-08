008fd6a7939b06ebd325b53c3d8a3d5ae1df9508
kebasyaty <kebasyaty@gmail.com>
2024-06-08 14:48:49 +0300

    add compose.cr to Home

---
+++ src/my_site/services/home/compose.cr
@@ -0,0 +1,20 @@
+# Service composition for menu structure of admin panel.
+module Vizbor::Services::Home
+  struct Compose < Vizbor::Composition::AdminPanelMenu
+    def self.composition : Vizbor::Composition::Service
+      # WARNING: Get icon name (for service) - https://materialdesignicons.com/
+      {
+        service:     {title: "???", icon: "help"},
+        collections: [
+          {
+            title:     "???",
+            model_key: "???", # Vizbor::Services::<ServiceName>::Models::<ModelName>.full_model_name,
+            fields:    [
+              {field: "???", title: "???"},
+            ],
+          },
+        ],
+      }
+    end
+  end
+end
