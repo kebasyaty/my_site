f784404e7c91941b5db0fbcc1a36077f3891442e
kebasyaty <kebasyaty@gmail.com>
2024-06-08 08:19:59 +0300

    update

--- src/my_site/services/admin/compose.cr
+++ src/my_site/services/admin/compose.cr
@@ -1,12 +1,21 @@
 # Service composition for the admin panel menu structure.
 module Vizbor::Services::Admin::Compose
   composition = {
-    service:     {title: "", icon: ""},
+    service:     {title: "Accounts", icon: "account-multiple"},
     collections: [
       {
-        title:     "",
-        model_key: "",
-        fields:    [{field: "", title: ""}],
+        title:     "Users",
+        model_key: Vizbor::Services::Admin::Models::User.full_model_name,
+        fields:    [
+          {field: "username", title: "Nickname"},
+          {field: "avatar", title: "Avatar"},
+          {field: "first_name", title: "First name"},
+          {field: "last_name", title: "Last name"},
+          {field: "email", title: "E-mail"},
+          {field: "phone", title: "Phone"},
+          {field: "is_admin", title: "Admin?"},
+          {field: "is_active", title: "Active?"},
+        ],
       },
     ],
   }
