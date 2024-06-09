682b18d32ec01eebcffc338fa50635764bb3e1c9
kebasyaty <kebasyaty@gmail.com>
2024-06-09 13:51:17 +0300

    update

--- src/my_site/services/admin/routes/service_list.cr
+++ src/my_site/services/admin/routes/service_list.cr
@@ -3,28 +3,36 @@ module Vizbor::Services::Admin::Routes
   post "/admin/service-list" do |env|
     authenticated? : Bool = false
     msg_err : String = ""
+    lang_code : String = Vizbor::Settings.default_locale
 
     if !(user = env.session.object?("user")).nil?
       user = user.as(Vizbor::Session::UserStorableObject)
       if !user.username.empty? &&
          !user.hash.empty? && user.is_admin? && user.is_active?
+        lang_code = user.lang_code
         authenticated? = true
       else
-        msg_err = I18n.t(:auth_failed)
+        I18n.with_locale(lang_code) do
+          msg_err = I18n.t(:auth_failed)
+        end
       end
     else
-      msg_err = I18n.t(:auth_failed)
+      I18n.with_locale(lang_code) do
+        msg_err = I18n.t(:auth_failed)
+      end
     end
 
-    result = {
-      is_authenticated: authenticated?,
-      logo:             "",
-      brand:            "Brand ???",
-      slogan:           "Slogan ???",
-      service_list:     Vizbor::Compose.get,
-      msg_err:          msg_err,
-    }.to_json
-    env.response.content_type = "application/json"
-    result
+    I18n.with_locale(lang_code) do
+      result = {
+        is_authenticated: authenticated?,
+        logo:             "",
+        brand:            "Brand ???",
+        slogan:           "Slogan ???",
+        service_list:     Vizbor::Compose.get,
+        msg_err:          msg_err,
+      }.to_json
+      env.response.content_type = "application/json"
+      return result
+    end
   end
 end
