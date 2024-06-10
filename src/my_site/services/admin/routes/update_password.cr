7f0805e8ae0cbe0bdbc13a65c0387d9a17f0d1f8
kebasyaty <kebasyaty@gmail.com>
2024-06-10 18:48:04 +0300

    update

--- src/my_site/services/admin/routes/update_password.cr
+++ src/my_site/services/admin/routes/update_password.cr
@@ -4,7 +4,6 @@ module Vizbor::Services::Admin::Routes
     authenticated? : Bool = false
     lang_code : String = Vizbor::Settings.default_locale
     msg_err : String = ""
-    username : String = env.params.json["username"].as(String)
     old_pass : String = env.params.json["old_pass"].as(String)
     new_pass : String = env.params.json["new_pass"].as(String)
     model_key : String = env.params.json["model_key"].as(String)
@@ -25,7 +24,7 @@ module Vizbor::Services::Admin::Routes
       if model_key == Vizbor::Services::Admin::Models::User.full_model_name
         halt env, status_code: 400, response: "Missing document hash." if doc_hash.empty?
         halt env, status_code: 400, response: "Invalid document hash." unless Valid.mongo_id?(doc_hash)
-        filter = {"_id": BSON::ObjectId.new(doc_hash), username: username}
+        filter = {"_id": BSON::ObjectId.new(doc_hash)}
         if user = Vizbor::Services::Admin::Models::User.find_one_to_instance(filter)
           begin
             user.update_password(
