766be1a1eb9a7c2e22b295990c32ae00125142c2
kebasyaty <kebasyaty@gmail.com>
2024-06-10 13:00:35 +0300

    update

--- src/my_site/services/admin/models/site_params.cr
+++ src/my_site/services/admin/models/site_params.cr
@@ -5,6 +5,11 @@ module Vizbor::Services::Admin::Models
     delete_doc?: false,
   )]
   struct SiteParameters < DynFork::Model
+    getter brand = DynFork::Fields::TextField.new(
+      label: I18n.t(:brand),
+      placeholder: I18n.t(:enter_company_name),
+      maxlength: 150,
+    )
     getter email = DynFork::Fields::EmailField.new(
       label: I18n.t(:email_for_feedback),
       placeholder: I18n.t(:enter_email),
