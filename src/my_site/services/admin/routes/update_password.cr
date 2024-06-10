5d03390135ebe11a12b2a892bac2cdef0d192198
kebasyaty <kebasyaty@gmail.com>
2024-06-10 17:12:09 +0300

    update

--- src/my_site/services/admin/routes/update_password.cr
+++ src/my_site/services/admin/routes/update_password.cr
@@ -3,6 +3,12 @@ module Vizbor::Services::Admin::Routes
   post "/admin/update-password" do |env|
     authenticated? : Bool = false
     lang_code : String = Vizbor::Settings.default_locale
+    msg_err : String = ""
+    username : String = env.params.json["username"].as(String)
+    old_pass : String = env.params.json["old_pass"].as(String)
+    new_pass : String = env.params.json["new_pass"].as(String)
+    model_key : String = env.params.json["model_key"].as(String)
+    doc_hash : String = env.params.json["doc_hash"].as(String)
 
     # User authentication
     if !(user = env.session.object?("user")).nil?
@@ -10,6 +16,8 @@ module Vizbor::Services::Admin::Routes
       if !user.username.empty? &&
          !user.hash.empty? && user.is_admin? && user.is_active?
         authenticated? = true
+      else
+        msg_err = I18n.t(:auth_failed)
       end
     end
 
@@ -18,7 +26,7 @@ module Vizbor::Services::Admin::Routes
       result = {
         is_authenticated: authenticated?,
         service_list:     Vizbor::Compose.get,
-        msg_err:          authenticated? ? "" : I18n.t(:auth_failed),
+        msg_err:          msg_err,
       }.to_json
     end
     env.response.content_type = "application/json"
