d7d575fd6c63d587de08bd17ba71ccc2d57ac99a
kebasyaty <kebasyaty@gmail.com>
2024-06-10 11:01:52 +0300

    fix

--- src/my_site/services/admin/routes/lang_code.cr
+++ src/my_site/services/admin/routes/lang_code.cr
@@ -1,6 +1,6 @@
 module Vizbor::Services::Admin::Routes
   # Get language code
-  get "/admin/lang_code" do |env|
+  get "/admin/lang-code" do |env|
     lang_code = {
       lang_code: Vizbor::Settings.default_locale,
       msg_err:   "",
