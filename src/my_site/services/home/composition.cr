ff7f853921c0ebd1713eed2cdbe7c09ca53c0c37
kebasyaty <kebasyaty@gmail.com>
2024-06-13 15:02:20 +0300

    update

--- src/my_site/services/home/composition.cr
+++ src/my_site/services/home/composition.cr
@@ -1,7 +1,7 @@
 # Service composition for menu structure of admin panel.
 module Vizbor::Services::Home
-  struct HomePage < Vizbor::Compose
-    def self.composition : Vizbor::Composition
+  struct HomePage < Vizbor::Сomposition
+    def self.composition : Vizbor::CompositionType
       # For example, look at 'compose.cr' in Vizbor::Services::Admin.
       # WARNING: Get icon name (for service) - https://materialdesignicons.com/
       {
