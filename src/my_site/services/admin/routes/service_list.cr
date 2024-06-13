87fd71d4bd8ed2f4b73a212799828fe4eacbe58c
kebasyaty <kebasyaty@gmail.com>
2024-06-13 13:30:04 +0300

    update

--- src/my_site/services/admin/routes/service_list.cr
+++ src/my_site/services/admin/routes/service_list.cr
@@ -1,23 +1,15 @@
 module Vizbor::Services::Admin::Routes
   # Get service list
   post "/admin/service-list" do |env|
-    admin_authenticated? : Bool = false
+    authenticated? : Bool = Vizbor::Globals::Auth.user_authenticated? env, is_admin?: true
     lang_code : String = Vizbor::Settings.default_locale
-
-    # User authentication
-    if !(user = env.session.object?("user")).nil?
-      user = user.as(Vizbor::Middleware::Session::UserStorableObject)
-      if user.is_admin? && user.is_active?
-        admin_authenticated? = true
-      end
-    end
 
     result : String? = nil
     I18n.with_locale(lang_code) do
       result = {
-        is_authenticated: admin_authenticated?,
+        is_authenticated: authenticated?,
         service_list:     Vizbor::Compose.get,
-        msg_err:          admin_authenticated? ? "" : I18n.t(:auth_failed),
+        msg_err:          authenticated? ? "" : I18n.t(:auth_failed),
       }.to_json
     end
     env.response.content_type = "application/json"
