cf39b00e27eb6d7cc12b974434d2915378c7cfeb
kebasyaty <kebasyaty@gmail.com>
2024-06-14 10:04:23 +0300

    update

--- src/my_site/services/admin/routes/service_list.cr
+++ src/my_site/services/admin/routes/service_list.cr
@@ -1,9 +1,9 @@
 module Vizbor::Services::Admin::Routes
   # Get service list
   post "/admin/service-list" do |env|
+    lang_code : String = env.session.string("current_lang")
     auth = Vizbor::Globals::Auth.user_authenticated? env, is_admin?: true
     authenticated? : Bool = auth[:authenticated?]
-    lang_code : String = Vizbor::Settings.default_locale
 
     result : String? = nil
     I18n.with_locale(lang_code) do
