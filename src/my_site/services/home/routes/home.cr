8461dff52b8922058db82c90f380ba2051a37265
kebasyaty <kebasyaty@gmail.com>
2024-06-16 12:53:53 +0300

    update

--- src/my_site/services/home/routes/home.cr
+++ src/my_site/services/home/routes/home.cr
@@ -2,10 +2,12 @@ module Vizbor::Services::Home::Routes
   # Home page
   get "/" do |env|
     _lang_code = Vizbor::Settings.default_locale
-    _meta_title = ""
-    _meta_description = ""
-    _brand = ""
-    _slogan = ""
+    basic_params = Vizbor::Services::Admin::Models::BasicSettings.find_one_to_instance
+    _brand = basic_params.brand
+    _slogan = basic_params.slogan
+    home_params = Vizbor::Services::Home::Models::HomePageSettings.find_one_to_instance
+    _meta_title = home_params.meta_title
+    _meta_description = home_params.meta_description
     _content = ""
     _styles = Array(String).new
     _scripts = Array(String).new
