a7e21876323878666ec2775d5b50bcdababb8d85
kebasyaty <kebasyaty@gmail.com>
2024-06-16 13:18:22 +0300

    update

--- src/my_site/services/home/routes/home.cr
+++ src/my_site/services/home/routes/home.cr
@@ -2,15 +2,13 @@ module Vizbor::Services::Home::Routes
   # Home page
   get "/" do |env|
     _lang_code = Vizbor::Settings.default_locale
-    basic_params = Vizbor::Services::Admin::Models::BasicSettings.find_one_to_instance
-    basic_params = basic_params.not_nil!
-    _brand = basic_params.brand.value
-    _slogan = basic_params.slogan.value
-    home_params = Vizbor::Services::Home::Models::HomePageSettings.find_one_to_instance
-    home_params = home_params.not_nil!
-    _meta_title = home_params.meta_title.value
-    _meta_description = home_params.meta_description.value
-    _content = ""
+    basic_params = Vizbor::Services::Admin::Models::BasicSettings.find_one_to_hash.not_nil!
+    _brand = basic_params["brand"]
+    _slogan = basic_params["slogan"]
+    home_params = Vizbor::Services::Home::Models::HomePageSettings.find_one_to_hash.not_nil!
+    _meta_title = home_params["meta_title"]
+    _meta_description = home_params["meta_description"]
+    _content = "Some content ..."
     _styles = Array(String).new
     _scripts = Array(String).new
     env.response.content_type = "text/html"
