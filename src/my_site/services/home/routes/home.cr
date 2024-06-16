d956b847848275426a29c773d1a3a4f2540b8eaa
kebasyaty <kebasyaty@gmail.com>
2024-06-16 13:30:34 +0300

    update

--- src/my_site/services/home/routes/home.cr
+++ src/my_site/services/home/routes/home.cr
@@ -5,7 +5,7 @@ module Vizbor::Services::Home::Routes
     basic_params = Vizbor::Services::Admin::Models::BasicSettings.find_one_to_hash.not_nil!
     _brand = basic_params["brand"]
     _slogan = basic_params["slogan"]
-    _contact_email = basic_params["slogan"]
+    _contact_email = basic_params["contact_email"]
     _contact_phone = basic_params["contact_phone"]
     home_params = Vizbor::Services::Home::Models::HomePageSettings.find_one_to_hash.not_nil!
     _meta_title = home_params["meta_title"]
