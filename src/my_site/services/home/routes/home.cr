26686a63423e4e6085bd095d12907a6513503554
kebasyaty <kebasyaty@gmail.com>
2024-06-16 12:07:33 +0300

    update

--- src/my_site/services/home/routes/home.cr
+++ src/my_site/services/home/routes/home.cr
@@ -1,8 +1,15 @@
 module Vizbor::Services::Home::Routes
   # Home page
-  get "/" do
-    _lang_code =
-      env.response.content_type = "text/html"
+  get "/" do |env|
+    _lang_code = ""
+    _meta_title = ""
+    _meta_description = ""
+    _brand = ""
+    _slogan = ""
+    _content = ""
+    _styles = Array(String).new
+    _scripts = Array(String).new
+    env.response.content_type = "text/html"
     render "views/layouts/home.html.ecr"
   end
 end
