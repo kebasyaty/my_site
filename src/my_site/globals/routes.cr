7aa79885c6fef1b0b1770ac1248d01d970727db7
kebasyaty <kebasyaty@gmail.com>
2024-06-16 10:01:21 +0300

    update

--- src/my_site/globals/routes.cr
+++ src/my_site/globals/routes.cr
@@ -7,7 +7,7 @@ module Vizbor::Globals::Routes
     _host = "#{Vizbor::Settings.host}:#{Vizbor::Settings.port}"
     _scheme = Vizbor::Settings.scheme
     env.response.content_type = "text/plain"
-    render "views/robots.txt"
+    render "views/robots.ecr"
   end
 
   get "/sitemap.xml" do |env|
@@ -18,7 +18,7 @@ module Vizbor::Globals::Routes
       priority: Float64,
     )) = [{loc: "test_loc", lastmod: "test_lastmod", changefreq: "test_changefreq", priority: 0.5}]
     env.response.content_type = "application/xml"
-    render "views/sitemap.xml"
+    render "views/sitemap.ecr"
   end
 
   error 404 do |env|
