312769fe09e1285d34e612d14a707ee23f44d24b
kebasyaty <kebasyaty@gmail.com>
2024-06-17 09:33:17 +0300

    update

--- src/my_site/services/home/routes/home.cr
+++ src/my_site/services/home/routes/home.cr
@@ -6,12 +6,12 @@ module Vizbor::Services::Home::Routes
     home_params = Vizbor::Services::Home::Models::HomePageSettings.find_one_to_hash.not_nil!
     Vizbor::Renders.base(
       lang_code: Vizbor::Settings.default_locale,
-      brand: basic_params["brand"],
-      slogan: basic_params["slogan"],
-      contact_email: basic_params["contact_email"],
-      contact_phone: basic_params["contact_phone"],
-      meta_title: home_params["meta_title"],
-      meta_description: home_params["meta_description"],
+      brand: basic_params["brand"].to_s,
+      slogan: basic_params["slogan"].to_s,
+      contact_email: basic_params["contact_email"].to_s,
+      contact_phone: basic_params["contact_phone"].to_s,
+      meta_title: home_params["meta_title"].to_s,
+      meta_description: home_params["meta_description"].to_s,
       content: "Some content ...",
       styles: Array(String).new,
       scripts: Array(String).new,
