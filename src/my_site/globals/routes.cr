61b0a955551885321c18632123e2396591a66f04
kebasyaty <kebasyaty@gmail.com>
2024-06-18 12:39:37 +0300

    update

--- src/my_site/globals/routes.cr
+++ src/my_site/globals/routes.cr
@@ -18,20 +18,51 @@ module Vizbor::Globals::Routes
     )
   end
 
-  # Logout
-  post "/logout" do |env|
-    auth = Vizbor::Globals::Auth.user_authenticated? env, is_admin?: true
-    if auth[:authenticated?]
-      env.session.destroy
-    end
-    env.redirect "/"
-  end
-
   # Change current language
   get "/change-current-lang/:lang_code" do |env|
     lang_code = env.params.url["lang_code"]
     env.session.string("current_lang", lang_code)
     env.redirect "/"
+  end
+
+  # Login
+  post "/login" do |env|
+    lang_code : String = env.session.string("current_lang")
+    auth = Vizbor::Globals::Auth.user_authenticated? env, is_admin?: true
+    authenticated? : Bool = auth[:authenticated?]
+    # Login form data
+    login : String = env.params.json["login"].as(String) # username or email
+    password : String = env.params.json["password"].as(String)
+
+    # Check if the user is authenticated?
+    unless authenticated?
+      auth = Vizbor::Globals::Auth.user_authentication(
+        env,
+        login: login,
+        password: password,
+        is_admin?: true,
+      )
+      authenticated? = auth[:authenticated?]
+    end
+
+    result : String? = nil
+    I18n.with_locale(lang_code) do
+      result = {
+        is_authenticated: authenticated?,
+        msg_err:          authenticated? ? "" : I18n.t(:auth_failed),
+      }.to_json
+    end
+    env.response.content_type = "application/json"
+    result
+  end
+
+  # Logout
+  post "/logout" do |env|
+    auth = Vizbor::Globals::Auth.user_authenticated? env
+    if auth[:authenticated?]
+      env.session.destroy
+    end
+    env.redirect "/login"
   end
 
   error 404 do |env|
