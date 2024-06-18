2c2fe86f8727325c8836a2818993c5a73fad2a41
kebasyaty <kebasyaty@gmail.com>
2024-06-18 15:20:26 +0300

    update

--- src/my_site/middleware/headers.cr
+++ src/my_site/middleware/headers.cr
@@ -17,7 +17,7 @@ module Vizbor::Middleware
       "Strict-Transport-Security",
       !Vizbor::Settings.debug? ? "max-age=31536000; includeSubDomains; preload" : "max-age=0",
     )
-    response.headers.add("Referrer-Policy", "strict-origin-when-cross-origin")
+    response.headers.add("Referrer-Policy", !Vizbor::Settings.debug? ? "strict-origin-when-cross-origin" : "unsafe-url")
     response.headers.add(
       "Content-Security-Policy",
       "Content-Security-Policy" \
