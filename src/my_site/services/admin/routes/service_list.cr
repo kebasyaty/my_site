8fb69325263450a1005be3790d0f4133a3217e58
kebasyaty <kebasyaty@gmail.com>
2024-06-19 07:45:54 +0300

    update

--- src/my_site/services/admin/routes/service_list.cr
+++ src/my_site/services/admin/routes/service_list.cr
@@ -4,16 +4,12 @@ module Vizbor::Services::Admin::Routes
     lang_code : String = env.session.string("current_lang")
     auth = Vizbor::Globals::Auth.user_authenticated? env, is_admin?: true
     authenticated? : Bool = auth[:authenticated?]
-    basic_params = Vizbor::Services::Admin::Models::SiteParams.find_one_to_instance
-    if basic_params.nil?
-      halt env, status_code: 400, response: "Basic settings are missing."
-    end
-
+    site_params = Vizbor::Services::Admin::Models::SiteParams.find_one_to_instance.not_nil!
     result : String? = nil
     I18n.with_locale(lang_code) do
       result = {
-        brand:            basic_params.brand.value,
-        slogan:           basic_params.slogan.value,
+        brand:            site_params.brand.value,
+        slogan:           site_params.slogan.value,
         is_authenticated: authenticated?,
         service_list:     Vizbor::MenuComposition.get,
         msg_err:          authenticated? ? "" : I18n.t(:auth_failed),
