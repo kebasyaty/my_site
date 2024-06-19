acdbf32d4deb54f8e6f3b8928e1ae3468cdfe647
kebasyaty <kebasyaty@gmail.com>
2024-06-19 08:14:13 +0300

    update

--- src/my_site/services/admin/routes/service_list.cr
+++ src/my_site/services/admin/routes/service_list.cr
@@ -5,16 +5,13 @@ module Vizbor::Services::Admin::Routes
     auth = Vizbor::Globals::Auth.user_authenticated? env, is_admin?: true
     authenticated? : Bool = auth[:authenticated?]
     site_params = Vizbor::Services::Admin::Models::SiteParams.find_one_to_hash.not_nil!
-    result : String? = nil
-    I18n.with_locale(lang_code) do
-      result = {
-        brand:            site_params["brand"],
-        slogan:           site_params["slogan"],
-        is_authenticated: authenticated?,
-        service_list:     Vizbor::MenuComposition.get,
-        msg_err:          authenticated? ? "" : I18n.t(:auth_failed),
-      }.to_json
-    end
+    result = {
+      brand:            site_params["brand"],
+      slogan:           site_params["slogan"],
+      is_authenticated: authenticated?,
+      service_list:     Vizbor::MenuComposition.get,
+      msg_err:          authenticated? ? "" : I18n.with_locale(lang_code) { I18n.t(:auth_failed) },
+    }.to_json
     env.response.content_type = "application/json"
     result
   end
