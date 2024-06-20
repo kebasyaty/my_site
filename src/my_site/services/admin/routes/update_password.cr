2b35b16bedab792067ac4903fe08f7c09d24e8df
kebasyaty <kebasyaty@gmail.com>
2024-06-20 08:37:32 +0300

    update

--- src/my_site/services/admin/routes/update_password.cr
+++ src/my_site/services/admin/routes/update_password.cr
@@ -2,7 +2,7 @@ module Services::Admin::Routes
   # Update user password via admin panel.
   post "/admin/update-password" do |env|
     lang_code : String = env.session.string("current_lang")
-    auth = Globals::Auth.user_authenticated? env, is_admin?: true
+    auth = Globals::Auth.user_authenticated? env
     authenticated? : Bool = auth[:authenticated?]
     msg_err : String = ""
     old_pass : String = env.params.json["old_pass"].as(String)
@@ -16,7 +16,7 @@ module Services::Admin::Routes
         halt env, status_code: 400, response: "Missing document hash." if doc_hash.empty?
         halt env, status_code: 400, response: "Invalid document hash." unless Valid.mongo_id?(doc_hash)
         filter = {"_id": BSON::ObjectId.new(doc_hash)}
-        if user = Vizbor::Services::Admin::Models::User.find_one_to_instance(filter)
+        if user = Services::Admin::Models::User.find_one_to_instance(filter)
           I18n.with_locale(lang_code) do
             begin
               user.update_password(
