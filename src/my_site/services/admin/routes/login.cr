7a18d19e9b97c3784bc0dc646bdc58663b57a18e
kebasyaty <kebasyaty@gmail.com>
2024-06-20 17:43:42 +0300

    update

--- src/my_site/services/admin/routes/login.cr
+++ src/my_site/services/admin/routes/login.cr
@@ -1,38 +1,34 @@
 module Services::Admin::Routes
   # Login page
   get "/admin/sign-in" do |env|
-    lang_code : String = env.session.string("current_lang")
-    auth = Globals::Auth.user_authenticated? env, lang_code
-    unless auth[:is_authenticated]
-      if Services::Admin::Models::User.estimated_document_count == 0
-        # Create first user (administrator)
-        first_user = Services::Admin::Models::User.new
-        first_user.username.value = "admin"
-        first_user.email.value = if !Vizbor::Settings.debug?
-                                   # by default for prod
-                                   Vizbor::Settings.admin_prod_email
-                                 else
-                                   # by default for dev
-                                   "no_reply@email.net"
-                                 end
-        first_user.password.value = if !Vizbor::Settings.debug?
-                                      # by default for prod
-                                      Vizbor::Settings.admin_prod_pass
-                                    else
-                                      # by default for dev
-                                      "12345678"
-                                    end
-        first_user.confirm_password.value = first_user.password.value
-        first_user.is_admin.value = true
-        first_user.is_active.value = true
+    if Services::Admin::Models::User.estimated_document_count == 0
+      # Create first user (administrator)
+      first_user = Services::Admin::Models::User.new
+      first_user.username.value = "admin"
+      first_user.email.value = if !Vizbor::Settings.debug?
+                                 # by default for prod
+                                 Vizbor::Settings.admin_prod_email
+                               else
+                                 # by default for dev
+                                 "no_reply@email.net"
+                               end
+      first_user.password.value = if !Vizbor::Settings.debug?
+                                    # by default for prod
+                                    Vizbor::Settings.admin_prod_pass
+                                  else
+                                    # by default for dev
+                                    "12345678"
+                                  end
+      first_user.confirm_password.value = first_user.password.value
+      first_user.is_admin.value = true
+      first_user.is_active.value = true
 
-        unless first_user.save
-          first_user.print_err
-          raise DynFork::Errors::Panic.new(
-            "Model : `Services::Admin::Models::User` => " +
-            "Error while creating the first user (administrator)."
-          )
-        end
+      unless first_user.save
+        first_user.print_err
+        raise DynFork::Errors::Panic.new(
+          "Model : `Services::Admin::Models::User` => " +
+          "Error while creating the first user (administrator)."
+        )
       end
     end
     send_file env, "templates/admin/index.html"
