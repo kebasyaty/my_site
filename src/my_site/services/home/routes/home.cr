679675b8c430269326e9275522c8dc28139d43bc
kebasyaty <kebasyaty@gmail.com>
2024-07-30 14:06:56 +0300

    update

--- src/my_site/services/home/routes/home.cr
+++ src/my_site/services/home/routes/home.cr
@@ -4,6 +4,7 @@ module Services::Home::Routes
     site_params = Services::Admin::Models::SiteParams.find_one_to_hash.not_nil!
     home_params = Services::Home::Models::HomePageParams.find_one_to_hash.not_nil!
     env.response.content_type = "text/html"
+    # NOTE: If necessary, create a custom render in Services::Home::Renders
     Globals::Renders.base(
       lang_code: Vizbor::Settings.default_locale, # or env.params.url["lang_code"]
       meta_title: home_params["meta_title"],
@@ -12,7 +13,7 @@ module Services::Home::Routes
         brand: site_params["brand"],
         slogan: site_params["slogan"],
       ),
-      content: Globals::Renders.base_content,
+      content: Services::Home::Renders.home_content,
       footer: Globals::Renders.base_footer(
         contact_email: site_params["contact_email"],
         contact_phone: site_params["contact_phone"],
