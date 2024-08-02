67f5578604293a8f630f321d423c215de59d8c68
kebasyaty <kebasyaty@gmail.com>
2024-08-02 11:14:42 +0300

    update

--- src/my_site/services/admin/routes/service_list.cr
+++ src/my_site/services/admin/routes/service_list.cr
@@ -2,6 +2,11 @@ module Services::Admin::Routes
   # Get service list
   post "/admin/service-list" do |env|
     lang_code : String = env.session.string("current_lang")
+    auth = Globals::Auth.user_authenticated? env, lang_code
+
+    unless auth[:is_authenticated] && auth[:is_admin]
+      halt env, status_code: 403, response: "Forbidden"
+    end
 
     result : String? = nil
     I18n.with_locale(lang_code) do
