94b7891cf8104d8c97351423b2bca58c39a33730
kebasyaty <kebasyaty@gmail.com>
2024-08-02 13:28:46 +0300

    update

--- src/my_site/services/admin/routes/service_list.cr
+++ src/my_site/services/admin/routes/service_list.cr
@@ -3,18 +3,20 @@ module Services::Admin::Routes
   post "/admin/service-list" do |env|
     lang_code : String = env.session.string("current_lang")
     auth = Globals::Auth.user_authenticated? env, lang_code
-
-    unless auth[:is_authenticated] && auth[:is_admin]
-      halt env, status_code: 403, response: "Forbidden"
-    end
+    authenticated? : Bool = auth[:is_authenticated] && auth[:is_admin]
 
     result : String? = nil
     I18n.with_locale(lang_code) do
       site_params = Services::Admin::Models::SiteParams.find_one_to_hash.not_nil!
       result = {
-        brand:        site_params["brand"],
-        slogan:       site_params["slogan"],
-        service_list: Vizbor::MenuComposition.get,
+        is_authenticated: auth[:is_authenticated],
+        brand:            site_params["brand"],
+        slogan:           site_params["slogan"],
+        service_list:     if authenticated?
+          Vizbor::MenuComposition.get
+        else
+          [] of Vizbor::MenuCompositionType
+        end,
       }.to_json
     end
     env.response.content_type = "application/json"
