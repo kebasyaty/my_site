5cc05542e10c78ef2dbe4988a2a903610ff93596
kebasyaty <kebasyaty@gmail.com>
2024-08-06 11:36:48 +0300

    update

--- src/my_site/globals/renders/extra/methods.cr
+++ src/my_site/globals/renders/extra/methods.cr
@@ -2,10 +2,18 @@
 module Globals::Extra::Methods
   # Get data to filter by category (сategory - selection type fields).
   def data_filters : Globals::Extra::Tools::AdminDataFilters
+    filter = [] of Globals::Extra::Tools::AdminDataFilters
     {% for var in @type.instance_vars %}
       if !@{{ var }}.ignored? && @{{ var }}.field_type.includes?("Choice")
-        # ...
+        filter << {
+          label: String,
+          field: String,
+          negation: false,
+          multiple: @{{ var }}.field_type.includes?("Mult"),
+          items: AdminDynItems,
+        }
       end
     {% end %}
+    filter
   end
 end
