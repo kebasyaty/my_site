fac641f1a343e72e1b73b518f49ec34532aeea6d
kebasyaty <kebasyaty@gmail.com>
2024-08-07 10:59:05 +0300

    update

--- src/my_site/globals/renders/extra/methods.cr
+++ src/my_site/globals/renders/extra/methods.cr
@@ -2,14 +2,14 @@
 module Globals::Extra::Methods
   # Generete a filter by categories (сategory - selection type fields).
   def admin_filter : Globals::Extra::Tools::AdminFilter
-    filter = [] of Globals::Extra::Tools::AdminFilter
+    filter = Globals::Extra::Tools::AdminFilter.new
     {% for var in @type.instance_vars %}
       if !@{{ var }}.ignored? && @{{ var }}.field_type.includes?("Choice")
         filter << {
           label: @{{ var }}.label,
           field: @{{ var }}.name,
           negation: false,
-          multiple: @{{ var }}.multiple,
+          multiple: @{{ var }}.multiple?,
           items: @{{ var }}.choices.map {|item| {value: item[0], title: item[1]}},
         }
       end
