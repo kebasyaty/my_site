ca4085663fd3494fefc60a78db3ae8b87dfaa480
kebasyaty <kebasyaty@gmail.com>
2024-08-08 10:26:21 +0300

    update

--- src/my_site/services/admin/routes/document_list.cr
+++ src/my_site/services/admin/routes/document_list.cr
@@ -6,22 +6,23 @@ module Services::Admin::Routes
     authenticated? : Bool = auth[:is_authenticated] && auth[:is_admin]
     #
     model_key = env.params.json["model_key"].as(String)
-    fields_name = env.params.json["fields_name"].as(Array(String))
-    page_num = env.params.json["page_num"].as(UInt32)
-    search_query = env.params.json["search_query"].as(String)
-    limit = env.params.json["limit"].as(UInt32)
-    sort = env.params.json["sort"].as(String)
-    direct = env.params.json["direct"].as(String)
-    filter = env.params.json["filter"].as(Globals::Extra::Tools::AdminFilter)
-    #
-    page_count : UInt32 = 1
-    documents = [] of Array(BSON)
-
     model = if target_model = Globals::Extra::Tools.target_model(model_key)
               target_model.not_nil!.new
             else
               raise Vizbor::Errors::Panic.new("There is no Model for `model_key`.")
             end
+    if authenticated?
+      fields_name = env.params.json["fields_name"].as(Array(String))
+      page_num = env.params.json["page_num"].as(UInt32)
+      search_query = env.params.json["search_query"].as(String)
+      limit = env.params.json["limit"].as(UInt32)
+      sort = env.params.json["sort"].as(String)
+      direct = env.params.json["direct"].as(String)
+      filter = env.params.json["filter"].as(Globals::Extra::Tools::AdminFilter)
+      #
+      page_count : UInt32 = 1
+      documents = [] of Array(BSON)
+    end
 
     result : String? = nil
     I18n.with_locale(lang_code) do
