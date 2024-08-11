ea38abf99c36dac978ea841236ca24214728b1c3
kebasyaty <kebasyaty@gmail.com>
2024-08-11 16:31:26 +0300

    update

--- src/my_site/services/admin/routes/document_list.cr
+++ src/my_site/services/admin/routes/document_list.cr
@@ -100,11 +100,7 @@ module Services::Admin::Routes
         skip: limit * (page_num - 1),
         limit: limit,
       )
-      page_count = ((model_class.count_documents(
-        filter,
-        skip: limit * (page_num - 1),
-        limit: limit,
-      ) / limit).ceil).to_i32
+      page_count = ((model_class.count_documents(filter) / limit).ceil).to_i32
     end
 
     result : String? = nil
