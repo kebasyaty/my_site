1c31c07753547e25a68833757572e6ac09579e86
kebasyaty <kebasyaty@gmail.com>
2024-08-12 13:31:26 +0300

    update

--- src/my_site/services/admin/routes/document_list.cr
+++ src/my_site/services/admin/routes/document_list.cr
@@ -10,12 +10,12 @@ module Services::Admin::Routes
     if authenticated?
       model_key = env.params.json["model_key"].as(String)
       model_class = Globals::Extra::Tools.model_class(model_key)
-      fields_name = env.params.json["fields_name"].as(Array(String))
+      fields_name = env.params.json["fields_name"].as(Array(JSON::Any)).map(&.to_s)
       field_name_params_list = model_class.meta[:field_name_params_list]
       field_name_params_list.select!(fields_name)
       search_query = env.params.json["search_query"].as(String)
-      page_num = env.params.json["page_num"].as(Int32)
-      limit = env.params.json["limit"].as(Int32)
+      page_num = env.params.json["page_num"].to_s.to_i32
+      limit = env.params.json["limit"].to_s.to_i32
       sort = env.params.json["sort"].as(String)
       direct = env.params.json["direct"].as(String)
       filter = BSON.new
