e3bf957ed521e18c0c416142e929071564657052
kebasyaty <kebasyaty@gmail.com>
2024-09-12 14:44:10 +0300

    update

--- src/my_site/services/admin/routes/lang_code.cr
+++ src/my_site/services/admin/routes/lang_code.cr
@@ -1,9 +1,16 @@
 module Services::Admin::Routes
-  # Get language code
+  # Get language code.
+  # Get theme and primary colors.
   get "/admin/lang-code" do |env|
+    lang_code : String = env.session.string("current_lang")
+    site_params = Services::Admin::Models::SiteParams.find_one_to_hash.not_nil!
+
     result = {
-      lang_code: env.session.string("current_lang"),
-      msg_err:   "",
+      lang_code:           lang_code,
+      dark_theme:          site_params["dark_theme"],
+      light_color_primary: site_params["light_color_primary"],
+      dark_color_primary:  site_params["dark_color_primary"],
+      msg_err:             "",
     }.to_json
     env.response.content_type = "application/json"
     result
