f873686860bcbef357113d3e8396175cb1dd6fbe
kebasyaty <kebasyaty@gmail.com>
2024-09-14 17:47:03 +0300

    update

--- src/my_site/services/admin/routes/lang_code.cr
+++ src/my_site/services/admin/routes/lang_code.cr
@@ -3,15 +3,23 @@ module Services::Admin::Routes
   # Get theme and primary colors.
   get "/admin/lang-code" do |env|
     lang_code : String = env.session.string("current_lang")
-    site_params = Services::Admin::Models::SiteParams.find_one_to_hash.not_nil!
+    auth = Globals::Auth.user_authenticated? env, lang_code
+    authenticated? : Bool = auth[:is_authenticated] && auth[:is_admin]
 
-    result = {
-      lang_code:           lang_code,
-      dark_theme:          site_params["dark_theme"],
-      light_color_primary: site_params["light_color_primary"],
-      dark_color_primary:  site_params["dark_color_primary"],
-      msg_err:             "",
-    }.to_json
+    result : String? = nil
+    I18n.with_locale(lang_code) do
+      site_params = Services::Admin::Models::SiteParams.find_one_to_hash.not_nil!
+      result = {
+        is_authenticated:    authenticated?,
+        lang_code:           lang_code,
+        dark_theme:          site_params["dark_theme"],
+        light_color_primary: site_params["light_color_primary"],
+        dark_color_primary:  site_params["dark_color_primary"],
+        brand:               site_params["brand"],
+        slogan:              site_params["slogan"],
+        msg_err:             "",
+      }.to_json
+    end
     env.response.content_type = "application/json"
     result
   end
