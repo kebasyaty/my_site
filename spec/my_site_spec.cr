f4c1c27ee0d1a22beb5b18c572f96cfab6f281b8
kebasyaty <kebasyaty@gmail.com>
2024-09-20 11:08:05 +0300

    update

--- spec/my_site_spec.cr
+++ spec/my_site_spec.cr
@@ -3,17 +3,17 @@ require "./spec_helper"
 describe MySite do
   it "works" do
     client : Mongo::Client = Mongo::Client.new
-    database_name = "test_pcSenRPaSdaU2IjZ"
+    database_name = "test_pcSenRPaSdaU6IjZ"
     collection_name = "test_collection_name"
     database = client[database_name]
     collection = database[collection_name]
-    collection.insert_one({one: 1})
+    collection.insert_one({"field_name_1" => [1, 2, 3]})
     filter = BSON.new
     tmp_doc = Array(BSON).new
-    tmp_doc << BSON.new({"one" => 1})
-    filter["$or"] = tmp_doc
+    tmp_doc << BSON.new({"field_name_1" => {"$all" => [1, 2, 3]}})
+    filter["$and"] = tmp_doc
     bson = collection.find_one(filter)
-    bson.not_nil!["one"].should eq(1)
+    bson.not_nil!.to_h["field_name_1"].should eq([1, 2, 3])
     client.close
   end
 end
