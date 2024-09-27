e2ff7555a6f4002e699533b85c08e8a3998b07df
kebasyaty <kebasyaty@gmail.com>
2024-09-27 08:46:10 +0300

    update

--- src/my_site/services/admin/routes/get_document.cr
+++ src/my_site/services/admin/routes/get_document.cr
@@ -4,5 +4,16 @@ module Services::Admin::Routes
     lang_code : String = env.session.string("current_lang")
     auth = Globals::Auth.user_authenticated? env, lang_code
     authenticated? : Bool = auth[:is_authenticated] && auth[:is_admin]
+
+    if authenticated?
+      model_key = env.params.json["model_key"].as(String)
+      doc_hash = env.params.json["doc_hash"].as(String)
+    end
+
+    result = {
+      is_authenticated: authenticated?,
+    }.to_json
+    env.response.content_type = "application/json"
+    result
   end
 end
