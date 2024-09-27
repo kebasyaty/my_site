c8a929892065224eb5c3957c7b0a9b7c84014faf
kebasyaty <kebasyaty@gmail.com>
2024-09-27 08:42:25 +0300

    update

---
+++ src/my_site/services/admin/routes/get_document.cr
@@ -0,0 +1,8 @@
+module Services::Admin::Routes
+  # Get document
+  post "/admin/get-document" do |env|
+    lang_code : String = env.session.string("current_lang")
+    auth = Globals::Auth.user_authenticated? env, lang_code
+    authenticated? : Bool = auth[:is_authenticated] && auth[:is_admin]
+  end
+end
