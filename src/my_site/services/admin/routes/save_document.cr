2fd4d63fd65c26e8ead3a6615b747ef31316a4c3
kebasyaty <kebasyaty@gmail.com>
2024-10-02 12:27:07 +0300

    update

--- src/my_site/services/admin/routes/save_document.cr
+++ src/my_site/services/admin/routes/save_document.cr
@@ -1,5 +1,5 @@
 module Services::Admin::Routes
-  # Get document
+  # Save/Update the document
   post "/admin/save-document" do |env|
     lang_code : String = env.session.string("current_lang")
     auth = Globals::Auth.user_authenticated? env, lang_code
