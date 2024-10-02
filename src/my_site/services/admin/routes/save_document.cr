629716f759fa5fcdebede4333bf3f925ec0eeecc
kebasyaty <kebasyaty@gmail.com>
2024-10-02 12:26:11 +0300

    update

---
+++ src/my_site/services/admin/routes/save_document.cr
@@ -0,0 +1,26 @@
+module Services::Admin::Routes
+  # Get document
+  post "/admin/save-document" do |env|
+    lang_code : String = env.session.string("current_lang")
+    auth = Globals::Auth.user_authenticated? env, lang_code
+    authenticated? : Bool = auth[:is_authenticated] && auth[:is_admin]
+    document = nil
+    msg_err : String = ""
+
+    if authenticated?
+      model_key = env.params.json["model_key"].as(String)
+      model_class = Globals::Extra::Tools.model_class(model_key)
+      data_form = env.params.json["data_form"].as(String)
+      instance = model_class.from_json(data_form)
+      instance.save
+    end
+
+    result = {
+      is_authenticated: authenticated?,
+      document:         instance,
+      msg_err:          msg_err,
+    }.to_json
+    env.response.content_type = "application/json"
+    result
+  end
+end
