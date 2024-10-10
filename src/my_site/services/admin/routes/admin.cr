4f32b5b0832fa4d6d6f0c1251831219fa4852db8
kebasyaty <kebasyaty@gmail.com>
2024-10-10 16:47:24 +0300

    update

--- src/my_site/services/admin/routes/admin.cr
+++ src/my_site/services/admin/routes/admin.cr
@@ -1,6 +1,13 @@
 module Services::Admin::Routes
   # Admin panel page
   get "/admin" do |env|
-    env.redirect "/admin/sign-in"
+    lang_code : String = env.session.string("current_lang")
+    auth = Globals::Auth.user_authenticated? env, lang_code
+    authenticated? : Bool = auth[:is_authenticated] && auth[:is_admin]
+    if authenticated?
+      send_file env, "templates/admin/index.html"
+    else
+      env.redirect "/admin/sign-in"
+    end
   end
 end
