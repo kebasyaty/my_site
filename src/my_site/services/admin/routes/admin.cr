488f3fa3146c8fbb8cf734122f17956db612a1cf
kebasyaty <kebasyaty@gmail.com>
2024-10-12 12:54:09 +0300

    update

--- src/my_site/services/admin/routes/admin.cr
+++ src/my_site/services/admin/routes/admin.cr
@@ -1,6 +1,30 @@
 module Services::Admin::Routes
-  # Admin panel page
+  # Home page
   get "/admin" do |env|
+    lang_code : String = env.session.string("current_lang")
+    auth = Globals::Auth.user_authenticated? env, lang_code
+    authenticated? : Bool = auth[:is_authenticated] && auth[:is_admin]
+    if authenticated?
+      send_file env, "templates/admin/index.html"
+    else
+      env.redirect "/admin/sign-in"
+    end
+  end
+
+  # Document list page
+  get "/admin/:service/:indexService/:collection/:indexCollection/document-list" do |env|
+    lang_code : String = env.session.string("current_lang")
+    auth = Globals::Auth.user_authenticated? env, lang_code
+    authenticated? : Bool = auth[:is_authenticated] && auth[:is_admin]
+    if authenticated?
+      send_file env, "templates/admin/index.html"
+    else
+      env.redirect "/admin/sign-in"
+    end
+  end
+
+  # Document page
+  get "/admin/:service/:indexService/:collection/:indexCollection/document/:indexDoc" do |env|
     lang_code : String = env.session.string("current_lang")
     auth = Globals::Auth.user_authenticated? env, lang_code
     authenticated? : Bool = auth[:is_authenticated] && auth[:is_admin]
