237c34fa5a254572d1f76b33cae804ea99606baf
kebasyaty <kebasyaty@gmail.com>
2024-10-17 13:16:08 +0300

    update

--- src/my_site/settings.cr
+++ src/my_site/settings.cr
@@ -28,7 +28,7 @@ module Vizbor::Settings
   class_getter app_name : String = "MySite"
   # WARNING: Match regular expression: /^[a-zA-Z0-9]{16}$/
   # NOTE: To generate a key (This is not an advertisement): https://randompasswordgen.com/
-  class_getter unique_app_key : String = "ZT2aB19G2UybAE2M"
+  class_getter unique_app_key : String = "I6u4280159Ql71s2"
   # WARNING: Maximum 60 characters.
   # WARNING: If the line is empty, the name will be generated automatically.
   class_getter database_name : String = ""
