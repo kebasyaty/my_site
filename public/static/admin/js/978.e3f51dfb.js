05329fbcfff3907478f8ddf62998f07067b500be
kebasyaty <kebasyaty@gmail.com>
2024-10-18 15:22:34 +0300

    update

--- public/static/admin/js/978.e3f51dfb.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkadmin_panel"]=self["webpackChunkadmin_panel"]||[]).push([[978],{978:function(n,e,l){l.r(e),l.d(e,{default:function(){return i}});var u=l(1750),a=function(){var n=this,e=n._self._c;return e(u.A)},t=[],r={name:"BlankPage"},s=r,c=l(1656),f=(0,c.A)(s,a,t,!1,null,null,null),i=f.exports}}]);
-//# sourceMappingURL=978.e3f51dfb.js.map
