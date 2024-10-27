40172c72f0078459b49cb19b4bcc57ef03afe35a
kebasyaty <kebasyaty@gmail.com>
2024-10-27 16:48:04 +0300

    update

--- src/my_site/services/admin/routes/get_document_list.cr
+++ src/my_site/services/admin/routes/get_document_list.cr
@@ -11,7 +11,7 @@ module Services::Admin::Routes
       model_key = env.params.json["model_key"].as(String)
       model_class = Globals::Extra::Tools.model_class(model_key)
       fields_name = env.params.json["fields_name"].as(Array(JSON::Any)).map(&.to_s)
-      field_name_params_list = model_class.meta[:field_name_params_list].select(fields_name)
+      field_name_params_list = model_class.meta[:field_name_params_list]
       search_query = env.params.json["search_query"].as(String)
       page_num = env.params.json["page_num"].to_s.to_i32
       limit = env.params.json["limit"].to_s.to_i32
@@ -129,7 +129,7 @@ module Services::Admin::Routes
         projection: projection,
         skip: limit * (page_num - 1),
         limit: limit,
-        field_name_params_list: field_name_params_list,
+        field_name_params_list: field_name_params_list.select(fields_name),
       )
       page_count = ((model_class.count_documents(filter) / limit).ceil).to_i32
     end
