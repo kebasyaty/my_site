1db3677784ac8e05ac8c7ed8cce60494b9f06099
kebasyaty <kebasyaty@gmail.com>
2024-11-07 13:36:41 +0200

    update

--- src/my_site/services/admin/routes/logout.cr
+++ src/my_site/services/admin/routes/logout.cr
@@ -2,8 +2,7 @@ module Services::Admin::Routes
   # Logout
   post "/admin/logout" do |env|
     env.session.destroy
-    result = {is_authenticated: false}.to_json
     env.response.content_type = "application/json"
-    result
+    "Goodbye!".to_json
   end
 end
