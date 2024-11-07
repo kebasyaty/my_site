8d2014e95922e09280f2b85e04b067d7a13ef9f9
kebasyaty <kebasyaty@gmail.com>
2024-11-07 14:06:25 +0200

    update

--- src/my_site/services/admin/routes/logout.cr
+++ src/my_site/services/admin/routes/logout.cr
@@ -1,7 +1,9 @@
 module Services::Admin::Routes
   # Logout
   post "/admin/logout" do |env|
+    lang_code : String = env.session.string("current_lang")
     env.session.destroy
+    env.session.string("current_lang", lang_code)
     env.response.content_type = "application/json"
     "Goodbye!".to_json
   end
