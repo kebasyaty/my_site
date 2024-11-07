4116bed499d572ec3017e0a981cb68584b85a40c
kebasyaty <kebasyaty@gmail.com>
2024-11-07 13:33:33 +0200

    update

--- src/my_site/services/admin/routes/logout.cr
+++ src/my_site/services/admin/routes/logout.cr
@@ -1,13 +1,8 @@
 module Services::Admin::Routes
   # Logout
   post "/admin/logout" do |env|
-    lang_code : String = env.session.string("current_lang")
-    auth = Globals::Auth.user_authenticated? env, lang_code
-    authenticated? : Bool = auth[:is_authenticated]
-    if authenticated?
-      env.session.destroy
-    end
-    result = {is_authenticated: authenticated?}.to_json
+    env.session.destroy
+    result = {is_authenticated: false}.to_json
     env.response.content_type = "application/json"
     result
   end
