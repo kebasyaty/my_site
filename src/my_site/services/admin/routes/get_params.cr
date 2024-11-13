88a28f6457f64122db40d6fd3f78e2fd025cf14c
kebasyaty <kebasyaty@gmail.com>
2024-11-13 12:12:52 +0200

    update

--- src/my_site/services/admin/routes/get_params.cr
+++ src/my_site/services/admin/routes/get_params.cr
@@ -4,19 +4,21 @@ module Services::Admin::Routes
     lang_code : String = env.session.string("current_lang")
     auth = Globals::Auth.user_authenticated? env, lang_code
     authenticated? : Bool = auth[:is_authenticated] && auth[:is_admin]
-
-    site_params = Services::Admin::Models::SiteParams.find_one_to_hash.not_nil!
-    logo = site_params["logo"].not_nil!.as(DynFork::Globals::Types::ImageData)
+    site_params = Services::Admin::Models::SiteParams.find_one_to_instance.not_nil!
     result = {
       is_authenticated: authenticated?,
       lang_code:        lang_code,
-      logo:             logo.url_md,
-      brand:            site_params["brand"] || "",
-      slogan:           site_params["slogan"] || "",
-      bg_image:         site_params["bg_image"] || "",
-      bg_opacity:       site_params["bg_opacity"],
-      dark_theme:       site_params["dark_theme"],
-      color_theme:      site_params["color_theme"],
+      logo:             site_params.logo.value.url_md,
+      brand:            site_params.brand.value || "",
+      slogan:           site_params.slogan.value || "",
+      bg_image:         if !(value = site_params.bg_image.value?).nil?
+        value.url
+      else
+        ""
+      end,
+      bg_opacity:  site_params.bg_opacity.value,
+      dark_theme:  site_params.dark_theme.value,
+      color_theme: site_params.color_theme.value,
     }.to_json
     env.response.content_type = "application/json"
     result
