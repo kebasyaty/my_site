2ad289fe60265393113202a4bd325f4ea244c035
kebasyaty <kebasyaty@gmail.com>
2024-11-13 10:06:35 +0200

    update

--- src/my_site/services/admin/models/parameters.cr
+++ src/my_site/services/admin/models/parameters.cr
@@ -34,18 +34,6 @@ module Services::Admin::Models
       placeholder: I18n.t(:enter_your_company_slogan),
       warning: [I18n.t(:apply_change_after_saving)],
     )
-    getter meta_title = DynFork::Fields::TextField.new(
-      label: I18n.t(:meta_title),
-      placeholder: I18n.t(:enter_meta_title),
-      maxlength: 60,
-      warning: [I18n.t(:for_meta_tag_title)],
-    )
-    getter meta_description = DynFork::Fields::TextField.new(
-      label: I18n.t(:meta_description),
-      placeholder: I18n.t(:enter_meta_description),
-      maxlength: 300,
-      warning: [I18n.t(:for_meta_tag_description)],
-    )
     getter contact_email = DynFork::Fields::EmailField.new(
       label: I18n.t(:email_for_feedback),
       placeholder: I18n.t(:enter_email),
