139b71493050d0d34902d7f6fd31491ddef91fcf
kebasyaty <kebasyaty@gmail.com>
2024-11-14 10:19:56 +0200

    update

--- src/my_site/services/home/routes/home.cr
+++ src/my_site/services/home/routes/home.cr
@@ -1,23 +1,22 @@
 module Services::Home::Routes
   # Home page
   get "/" do |env|
-    main_params = Services::Admin::Models::MainParams.find_one_to_hash.not_nil!
-    logo = main_params["logo"].not_nil!.as(DynFork::Globals::Types::ImageData)
+    main_params = Services::Admin::Models::MainParams.find_one_to_instance.not_nil!
     env.response.content_type = "text/html"
     # WARNING: If necessary, create a custom render in Services::Home::Renders
     Globals::Renders.base(
       lang_code: Vizbor::Settings.default_locale, # or env.params.url["lang_code"]
-      meta_title: main_params["meta_title"]?,
-      meta_description: main_params["meta_description"]?,
+      meta_title: "",
+      meta_description: "",
       header: Globals::Renders.base_header(
-        logo: logo.url_md,
-        brand: main_params["brand"],
-        slogan: main_params["slogan"],
+        logo: main_params.logo.value.url_md,
+        brand: main_params.brand.value,
+        slogan: main_params.slogan.value,
       ),
       content: Services::Home::Renders.home_content,
       footer: Globals::Renders.base_footer(
-        contact_email: main_params["contact_email"],
-        contact_phone: main_params["contact_phone"],
+        contact_email: main_paramscontact_email.value,
+        contact_phone: main_params.contact_phone.value,
       ),
       styles: Array(String).new,
       scripts: Array(String).new,
