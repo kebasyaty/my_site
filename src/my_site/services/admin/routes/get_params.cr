639cba167ca49026e16ca660b1bc1af045021c8b
kebasyaty <kebasyaty@gmail.com>
2024-11-15 10:54:51 +0200

    update

--- src/my_site/services/admin/routes/get_params.cr
+++ src/my_site/services/admin/routes/get_params.cr
@@ -5,20 +5,17 @@ module Services::Admin::Routes
     auth = Globals::Auth.user_authenticated? env, lang_code
     authenticated? : Bool = auth[:is_authenticated] && auth[:is_admin]
     main_params = Services::Admin::Models::MainParams.find_one_to_instance.not_nil!
+    img_val : DynFork::Globals::ImageData?
     result = {
       is_authenticated: authenticated?,
       lang_code:        lang_code,
-      logo:             main_params.logo.value.url_md,
+      logo:             (img_val.url_md if img_val = main_params.logo.value?) || "",
       brand:            main_params.brand.value? || "",
       slogan:           main_params.slogan.value? || "",
-      bg_image:         if value = main_params.bg_image.value?
-        value.url
-      else
-        ""
-      end,
-      bg_opacity:  main_params.bg_opacity.value,
-      dark_theme:  main_params.dark_theme.value,
-      color_theme: main_params.color_theme.value,
+      bg_image:         (img_val.url if img_val = main_params.bg_image.value?) || "",
+      bg_opacity:       main_params.bg_opacity.value,
+      dark_theme:       main_params.dark_theme.value,
+      color_theme:      main_params.color_theme.value,
     }.to_json
     env.response.content_type = "application/json"
     result
