dffa0899fe56e4bd7288258af4b6362c17569304
kebasyaty <kebasyaty@gmail.com>
2024-11-18 09:34:49 +0200

    update

--- src/app/middlewares/headers.cr
+++ src/app/middlewares/headers.cr
@@ -2,23 +2,23 @@
 module Vizbor::Middleware
   # Adds headers to Kemal::StaticFileHandler.
   # This is especially useful for stuff like CORS or caching.
-  static_headers do |response, _filepath, filestat|
+  static_headers do |env, _filepath, filestat|
     # Add CORS
-    response.headers.add("Access-Control-Allow-Origin", Vizbor::Settings.app_url)
-    response.headers.add("Access-Control-Allow-Methods", "GET")
-    response.headers.add("Access-Control-Allow-Headers", "authorization, accept, content-type")
-    response.headers.add("Access-Control-Max-Age", !Vizbor::Settings.debug? ? "3600" : "-1")
+    env.response.headers.add("Access-Control-Allow-Origin", Vizbor::Settings.app_url)
+    env.response.headers.add("Access-Control-Allow-Methods", "GET")
+    env.response.headers.add("Access-Control-Allow-Headers", "authorization, accept, content-type")
+    env.response.headers.add("Access-Control-Max-Age", !Vizbor::Settings.debug? ? "3600" : "-1")
     # Add Headers
-    response.headers.add("Content-Size", filestat.size.to_s)
-    response.headers.add("X-XSS-Protection", "1; mode=block")
-    response.headers.add("X-Frame-Options", "deny")
-    response.headers.add("X-Content-Type-Options", "nosniff")
-    response.headers.add(
+    env.response.headers.add("Content-Size", filestat.size.to_s)
+    env.response.headers.add("X-XSS-Protection", "1; mode=block")
+    env.response.headers.add("X-Frame-Options", "deny")
+    env.response.headers.add("X-Content-Type-Options", "nosniff")
+    env.response.headers.add(
       "Strict-Transport-Security",
       !Vizbor::Settings.debug? ? "max-age=31536000; includeSubDomains; preload" : "max-age=0",
     )
-    response.headers.add("Referrer-Policy", !Vizbor::Settings.debug? ? "strict-origin-when-cross-origin" : "unsafe-url")
-    response.headers.add(
+    env.response.headers.add("Referrer-Policy", !Vizbor::Settings.debug? ? "strict-origin-when-cross-origin" : "unsafe-url")
+    env.response.headers.add(
       "Content-Security-Policy",
       "default-src 'self';" \
       " connect-src 'self' ws: http%{s}:;" \
